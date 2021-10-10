8bcbedc70bea2a9eeb6119a675d80f7c4a89bb2f
EdgardH2305 <61916503+EdgardH2305@users.noreply.github.com>
2021-10-09 19:16:49 -0600

    Arreglos css y html

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.new-concept', { delay: 500 });
+ScrollReveal().reveal('.concept-banner-one', { delay: 500 });
+ScrollReveal().reveal('.concept-banner-two', { delay: 500 });
