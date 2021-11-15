6656c992a678b69d4e6418e91e8f3c45764ffbc3
ErnestoMontesino <62313726+ErnestoMontesino@users.noreply.github.com>
2021-11-15 11:21:53 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.new-concept', { delay: 500 });
+ScrollReveal().reveal('.concept-banner-one', { delay: 500 });
+ScrollReveal().reveal('.concept-banner-two', { delay: 500 });
+
